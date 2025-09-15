febcedc6bd8913bc59ea1d0ab8d828d57c3d5dfa
jarvis <manhasabhay000@gmail.com>
2025-09-16 00:12:33 +0530

    34

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-export default defineConfig({ plugins: [react()] })
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
