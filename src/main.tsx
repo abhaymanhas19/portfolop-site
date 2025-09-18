7a3b62be34c9f68911e897d5151389f789fa15dd
jarvis <manhasabhay000@gmail.com>
2025-09-18 11:26:15 +0530

    234

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";                 // ← keep this import
+import { createRoot } from "react-dom/client";
+import "./index.css";  // This imports Tailwind + your custom CSS
 import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode><App /></React.StrictMode>
+import { BrowserRouter } from "react-router-dom";
+ 
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
