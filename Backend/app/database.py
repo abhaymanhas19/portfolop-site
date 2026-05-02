5833d88243c28108ef4dbfa9cf7f7c1139085007
abhaymanhas19 <manhasabhay000@gmail.com>
2026-05-02 23:21:00 +0530

    db setting changes'; gi

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -6,11 +6,11 @@ from sqlmodel import SQLModel, create_engine, Session
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER", "abhaymanhas")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "portfolio")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 encoded_password = urllib.parse.quote_plus(DB_PASSWORD)
 
